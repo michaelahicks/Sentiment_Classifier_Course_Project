1093d3aad03ee57db8aba531a3772dd250d9e5b1
michaelahicks <113146164+michaelahicks@users.noreply.github.com>
2022-09-15 16:04:06 -0400

    Updated with functions that compare for postive and negative words and returns a count.

--- Sentiment_Classifier_Course_Project.py
+++ Sentiment_Classifier_Course_Project.py
@@ -1,6 +1,6 @@
 punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
 
-# function to strip puntuations from words passed to the function
+""" function to strip puntuations from words passed to the function """
 def strip_punctuation(word):
     print(word)
     for char in word:
@@ -8,3 +8,29 @@ def strip_punctuation(word):
             word = word.replace(char, "")
             print(word)
     return word
+
+
+"""function that takes words in a string and compares them to a postive words
+list and increments a counter for each one """
+
+def get_pos(string):
+    print(string)
+    pos = 0
+    words = string.lower().split()
+    for word in words:
+        s_word = strip_punctuation(word)
+        if s_word in positive_words:
+            pos = pos + 1
+    return pos
+
+"""function that takes words in a string and compares them to a negative words
+list and increments a counter for each one """
+def get_neg(string):
+    print(string)
+    neg = 0
+    words = string.lower().split()
+    for word in words:
+        s_word = strip_punctuation(word)
+        if s_word in negative_words:
+            neg = neg + 1
+    return neg
